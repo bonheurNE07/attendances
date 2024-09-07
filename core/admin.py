75791548957a8f0474dcb31e39fefe1d42eb0089
bonheurNE07 <bonheurndezenc@gmail.com>
2024-09-07 16:38:54 +0200

    Ignore folder my_folder

--- core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser
-
-@admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'photo')  # Customize as needed
-    search_fields = ('username', 'email')
-
